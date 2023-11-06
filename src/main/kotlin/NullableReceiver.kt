1e3c3bf7eb840274efa6b338e8194151bc6a9a97
Ek-star <totoekalma@gmail.com>
2023-11-07 01:20:33 +0700

    Upload NullableReceiver

--- src/main/kotlin/NullableReceiver.kt
+++ src/main/kotlin/NullableReceiver.kt
@@ -1,6 +1,14 @@
 fun main() {
-    val value: Int? = null
-    println(value.slice)
+    val value1: Int? = null
+    println(value1.slice)
+
+    // Lalu kapan kita membutuhkannya? Tentunya jika kita mempunyai sebuah objek yang bernilai null. Saat kita tidak menetapkannya dengan nullable receiver type, maka kita perlu memeriksa apakah objek tersebut bernilai null atau tidak? Bisa juga dengan menggunakan operator safe call setiap kali extension tersebut dipanggil
+
+    val value2: Int? = null
+    val value3: Int? = null
+
+    println(value2?.slice)
+    println(value3?.slice)
 }
 
 private val Int?.slice: Int
