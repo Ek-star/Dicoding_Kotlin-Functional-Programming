ce641e7bbf81a62a64c36b80e16f54950aaa8e27
Ek-star <totoekalma@gmail.com>
2023-11-06 15:50:19 +0700

    Upload Scope Function (Video)

--- src/main/kotlin/ScopeFuntion.kt
+++ src/main/kotlin/ScopeFuntion.kt
@@ -1,3 +1,14 @@
-fun main() {
+import java.lang.StringBuilder
 
+fun main() {
+    val dicoding = StringBuilder()
+    // dicoding.append("Dicoding")
+    // Append akan menambahkan (menyusun dan menambahkan menjadi sebuah kalimat)
+    dicoding.apply {
+        append("Dicoding")
+        append(" Academy")
+    }
+
+    println(dicoding)
+    // Ada banyak Jenis Scope Function
 }
