6c0d13b8625b8206742943d6406d6d677c737ee5
Ek-star <totoekalma@gmail.com>
2023-11-06 15:54:35 +0700

    Upload Collection Function (Video)

--- src/main/kotlin/CollectionFunction.kt
+++ src/main/kotlin/CollectionFunction.kt
@@ -1,3 +1,12 @@
 fun main() {
+    val dicoding = 1..10
+    println(dicoding.take(3))
+    // Output: [1, 2, 3]
 
+    // Jika cara manual menggunakan looping dan If statement:
+    for (i in dicoding) {
+        if (i < 4) {
+            print("$i ")
+        }
+    }
 }
