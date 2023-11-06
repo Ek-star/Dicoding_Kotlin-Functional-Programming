4950afcaf38d270ec100e66f2b729f236bcdbd61
Ek-star <totoekalma@gmail.com>
2023-11-06 15:29:59 +0700

    Upload High Order Function (Video)

--- src/main/kotlin/HighOrderFunction.kt
+++ src/main/kotlin/HighOrderFunction.kt
@@ -1,3 +1,29 @@
 fun main() {
+    printName("Ekalma Toto Alloy Sembiring") { value ->
+        val first = "Kotlin dipelajari Oleh"
+        "$first $value"
+    }
 
+    penjumlahanBilanganDenganDua(8) {it ->
+        it + 2
+    }
+
+    printResult(10, sum)
 }
+private fun printName(value: String, name: (String) -> String){
+    println(name(value))
+}
+
+private fun penjumlahanBilanganDenganDua(value: Int, jumlah: (Int) -> Int) {
+    println(jumlah(value))
+}
+
+private fun printResult(value: Int, sum: (Int) -> Int) {
+    val result = sum(value)
+    println(result)
+}
+
+private val sum: (Int) -> Int = { value ->
+    value + value
+}
+
