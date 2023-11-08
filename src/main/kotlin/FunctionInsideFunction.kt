5641c94a0781e78842f0c6840e5bdbcd9a1941d4
Ek-star <totoekalma@gmail.com>
2023-11-08 09:45:51 +0700

    Upload Scope Function Inside Function

--- src/main/kotlin/FunctionInsideFunction.kt
+++ src/main/kotlin/FunctionInsideFunction.kt
@@ -1,10 +1,26 @@
 fun main() {
     setWord("Ekalma")
+    println(sum(2 , 1 , 4))
 }
 
+// Contoh dari sebuah Inner Function
 private fun setWord(message: String) {
     fun printMessage(text: String) {
         println(text)
     }
     printMessage(message)
+}
+
+private fun sum(valueA: Int, valueB: Int, valueC: Int): Int {
+    fun validateNumber(value: Int) {
+        if (value == 0) {
+            throw IllegalArgumentException("value must be better than 0")
+        }
+    }
+
+    validateNumber(valueA)
+    validateNumber(valueB)
+    validateNumber(valueC)
+
+    return valueA + valueB + valueC
 }
